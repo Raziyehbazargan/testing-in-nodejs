ec4328df676c7603c61d7307d3592d39e3c5b9dc
raziyeh bazargan <r.bazargan@gmail.com>
2016-12-16 18:50:14 -0800

    TDD - second test Done

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -10,5 +10,18 @@ describe('Tags', function() {
       expect(results).to.have.a.property('depth', 4);
       expect(results).to.have.a.property('hello', 'world');
     });
+    it('should fallback to defaults', function() {
+      var args = ['--depth=4', '--hello=world'];
+      var defaults = { depth: 2, foo: 'bar' };
+      var results = tags.parse(args, defaults);
+
+      var expected = {
+        depth: 4,
+        foo: 'bar',
+        hello: 'world'
+      };
+      expect(results).to.deep.equal(expected);
+
+    });
   });
 });
