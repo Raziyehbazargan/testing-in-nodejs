ecb933984e0c9873e4f2444c55eb726bb7efae77
raziyeh bazargan <r.bazargan@gmail.com>
2016-12-24 22:21:38 -0800

    url-parameters

--- REST-SVC/routers/widget.js
+++ REST-SVC/routers/widget.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const widgetRouter = express.Router();
 
+const path = require('path');
+const db = require('../database').connect(path.join(__dirname, '..', 'widgets.json'));
+
 widgetRouter.route('/widgets')
   .get( function(req, res) {
-    res.json([
-      { name: 'widget 1', color: 'blue', size: 'large', quantity: 3},
-      { name: 'widget 2', color: 'red', size: 'medium', quantity: 5},
-      { name: 'widget 3', color: 'orange', size: 'small', quantity: 15},
-    ]);
+    db(con => con.getall().then(widgets => res.json(widgets)));
+
+    // res.json([
+    //   { name: 'widget 1', color: 'blue', size: 'large', quantity: 3},
+    //   { name: 'widget 2', color: 'red', size: 'medium', quantity: 5},
+    //   { name: 'widget 3', color: 'orange', size: 'small', quantity: 15},
+    // ]);
   })
   .post();
 
 widgetRouter.route('/widgets/:widgetId')
-  .get()
+  .get(function(req, res) {
+    db(con => con.get(parseInt(req.params.widgetId)).then(widgets => res.json(widgets)));
+  })
   .put()
-  .delete();
+  .delete(function(req, res) {
+    db(con => con.delete(parseInt(req.params.widgetId)).then(widgets => res.json(widgets)));
+  });
 
   module.exports = widgetRouter;
