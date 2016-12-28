a926b3c4a5e52a06a7e6e2c15a44fc8ec9428502
raziyeh bazargan <r.bazargan@gmail.com>
2016-12-28 14:24:14 -0800

    working with static files - express - add settings in package.json file

--- web-app/index.js
+++ web-app/index.js
@@ -1,3 +1,8 @@
+
+const fs = require('fs');
+
+const options = JSON.parse(fs.readFileSync('package.json'));
+
 const http = require('http');
 const express = require('express');
 
@@ -7,8 +12,8 @@ const app = express();
 const server = http.createServer(app);
 
 //serve the static files
-app.use(express.static('www'));
+app.use(express.static(options.webServer.folder));
 
-server.listen(3000, function() {
-  console.log('webserver started on port 3000.')
+server.listen(options.webServer.port, function() {
+  console.log(`webserver started on port ${options.webServer.port}`)
 })
